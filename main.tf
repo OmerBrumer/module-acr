c016f8411af8fe2159a7987e7e015335b5a928b5
Omer Brumer <brumeromer@gmail.com>
2023-02-02 16:09:53 +0200

    asd

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "azurerm_container_registry" "main" {
 }
 
 module "private_endpoint" {
-  source = "git::https://github.com/OmerBrumer/module-private-endpoint.git"
+  source = "git::https://github.com/OmerBrumer/module-private-endpoint.git?ref=dev"
 
   private_endpoint_name          = "${var.container_registry_config.name}-private-endpoint"
   resource_group_name            = var.resource_group_name
@@ -99,7 +99,7 @@ module "private_endpoint" {
 }
 
 module "diagnostic_settings" {
-  source = "git::https://github.com/OmerBrumer/module-diagnostic-setting.git"
+  source = "git::https://github.com/OmerBrumer/module-diagnostic-setting.git?ref=dev"
 
   diagonstic_setting_name    = "${azurerm_container_registry.main.name}-diagnostic-setting"
   log_analytics_workspace_id = var.log_analytics_workspace_id
